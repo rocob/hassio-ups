4893f6fd41bcb614163cceb3e24c9c8f7270fcd6
Róbert Kovaľ <67092916+rocob@users.noreply.github.com>
2020-12-27 21:48:25 +0100

    Update ina219ups.py

--- ina219ups.py
+++ ina219ups.py
@@ -3,11 +3,18 @@ from ina219 import INA219
 from ina219 import DeviceRangeError
 SHUNT_OHMS = 0.05
 
+from datetime import datetime
+#today = date.today()
+now = datetime.now()
+dt = now.strftime("%Y-%m-%d")
+tm = now.strftime("%H:%M")
 
 def read():
     """Define method to read information from coulometer."""
     ina = INA219(shunt_ohms=SHUNT_OHMS, busnum=1)
     ina.configure()
+    print("Date:", dt)
+    print("Time:", tm)
     print("Voltage: %.3f V" % ina.voltage())
     try:
         print("Current: %.3f mA" % ina.current())
